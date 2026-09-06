b840c2a3d2362b95145f656cfda040df5233d17b
SofiaTenghoff <tenghoffso@gmail.com>
2026-09-06 17:42:49 -0400

    Update FastAPI.py

--- API_Code/FastAPI.py
+++ API_Code/FastAPI.py
@@ -4,5 +4,15 @@
 #3. Run my C++ program on that file
 #4. Return the output that my program printed to cout
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 app = FastAPI()
+@app.get("/") #this is where you get the app
+def read_root():
+  return {"message": "Hello world"}
+@app.post("/upload") #creates a folder called upload where I will find what the user uploads
+async def final_location_for_files(uploaded_file: UploadFile):
+  content = await uploaded_file.read()
+  #process the content
+  print(content)
+  
+  

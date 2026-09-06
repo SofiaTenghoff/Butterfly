af0354da96d895d1b4b2f722de37deddffac0bae
SofiaTenghoff <tenghoffso@gmail.com>
2026-09-06 17:52:18 -0400

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -21,57 +21,26 @@ int main(int argc, char* argv[])
 	cout<<"M          Show this Menu "<<endl;
 	cout<<"Q          Quit Program"<<endl; */
 	
-	for(int i = 1; i < argc && choice != 'Q'; i++)
+	for(int i = 1; i < argc; i++)
 	{
 		//command line argument gets read here
-
-		choice = toupper(argv[i][0]);
-		switch(choice)
-		{
-			case 'I':
-			{
+			// if user choice = I
 				char filename[31];
 				// next command line argument, the filename, gets read here
-				++i;
 				strcpy(filename, argv[i]);
+			// the list changes when you import the file, that's where showlist and create report get their information from
 				if(!studentlist.ImportFile(filename))
 					cerr<<"bad file, task not completed"<<endl;
-			}
-			break;
-			case 'S':
-			{
+			
+			//if user choise = S
 				studentlist.ShowList();
-			}
-			break;
-			case 'E':
-			{
-				char filename[31];
-				// next command line argument, the filename, gets read here
-				++i;
-				strcpy(filename, argv[i]);
-				if(!studentlist.CreateReportFile(filename))
+	
+	
+				if(!studentlist.CreateReportFile(filename)) //they have to have chosen importFile BEFORE this step
 					cerr<<"Error creating report file\n";
 				else
 					cerr<<"Report successfully written to file";
-			}
-			break;
-			case 'M':
-			{
-				/*cout<<"           *** Student List menu ***"<<endl;
-			        cout<<"I          Import students from a file"<<endl;
-        			cout<<"S          Show student list (brief)"<<endl;
-        			cout<<"E          Export a grade report (to file)"<<endl;
-        			cout<<"M          Show this Menu "<<endl;
-        			cout<<"Q          Quit Program"<<endl;*/
-
-			}
-			break;	
-			case 'Q':
-			{
-			}
-			break;
-			default:
-				cerr<< "Invalid choice, please try again"<<endl;
+		
 		}
 	}
 	return 0;

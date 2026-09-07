17965650864495085b45db92b7963e0100ec7f3a
SofiaTenghoff <tenghoffso@gmail.com>
2026-09-07 13:32:11 -0400

    Update FastAPI.py

--- API_Code/FastAPI.py
+++ API_Code/FastAPI.py
@@ -5,14 +5,17 @@
 #4. Return the output that my program printed to cout
 
 from fastapi import FastAPI, File, UploadFile
-app = FastAPI()
-@app.get("/") #this is where you get the app
-def read_root():
-  return {"message": "Hello world"}
-@app.post("/upload") #creates a folder called upload where I will find what the user uploads
-async def final_location_for_files(uploaded_file: UploadFile):
-  content = await uploaded_file.read()
-  #process the content
-  print(content)
+
+app = FastAPI() # creates an instance of our application
+
+#create a route that responds to a get request at the root url
+@app.get("/") #this is where you get the app. The forward slash makes it for the home route
+
+#decorating a function called home with the @app.get decorator.
+#if you pass in a path, which is just a forward slash for the route, this function will run
+def home():
+  return {"message": "Hello world"} #fast api converts this dictionary to json automatically
+  
+
   
   

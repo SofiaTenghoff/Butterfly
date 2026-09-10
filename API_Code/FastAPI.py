2be05988b7db942e4ab6b2eb1ca7a05ef505390d
SofiaTenghoff <tenghoffso@gmail.com>
2026-09-10 16:47:14 -0400

    Update FastAPI.py

--- API_Code/FastAPI.py
+++ API_Code/FastAPI.py
@@ -5,11 +5,12 @@
 #4. Return the output that my program printed to cout
  
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
 
 app = FastAPI() # creates an instance of our application
 
 #create a route that responds to a get request at the root url
-@app.get("/") #this is where you get the app. The forward slash makes it for the home route
+@app.get("/", response_class = HTMLResponse) #this is where you get the app. The forward slash makes it for the home route
 
 #decorating a function called home with the @app.get decorator.
 #if you pass in a path, which is just a forward slash for the route, this function will run
@@ -31,6 +32,6 @@ posts: list[dict] = [
 #create an api endpoint for these dictionaries:
 @app.get("/api/posts") # creating a new route for the posts from the dictionaries
 def get_posts():
-  return posts
+  return f"<h1>{posts[0]['title']}</h1>"
   
   

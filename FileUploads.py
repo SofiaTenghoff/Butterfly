6bf66ff87d781959a520f33b02f4c97b01ec69d1
SofiaTenghoff <tenghoffso@gmail.com>
2026-09-11 12:47:48 -0400

    Update FileUploads.py

--- FileUploads.py
+++ FileUploads.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from fastapi import UploadFile
 import subprocess #lets Python run other files on your computer
 
-UPLOADED_FILE_DIR = Path("files/uploaded")
+CPP_EXECUTABLE = Path(r"C:\Users\tengh\Git\Butterfly")
 
 def process_grade_report_file():
   processed_file = subprocess.run(
-    ["./grade_processor", "some_file.txt"],
+    [str(FILE_DIR / "menu.exe"), str(FILE_DIR / "test1.txt")],
     capture_output=True,
     text=True
 )

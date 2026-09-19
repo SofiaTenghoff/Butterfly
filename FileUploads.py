3263168afff74c11f3b66573e0af6b14a79d37ea
SofiaTenghoff <tenghoffso@gmail.com>
2026-09-19 16:56:54 -0400

    Update FileUploads.py

--- FileUploads.py
+++ FileUploads.py
@@ -23,8 +23,7 @@ async def process_grade_report_file(file: UploadFile = File(...)): #file is a va
     #Remove UTF BOM (extra 3 bytes on windows) if present
     if content.startswith(b'\xef\xbb\xbf'):
       content = content[3:]
-    print("Uploaded content length:", len(content))
-    print("First 100 bytes:", content[:100])
+    
     tmp_in.write(content) #writes the uploaded file's data into a temporary file on the server
     tmp_in.flush() #force it to disk
     input_path = tmp_in.name #input_path is a normal string variable so we can access the temporary file later
